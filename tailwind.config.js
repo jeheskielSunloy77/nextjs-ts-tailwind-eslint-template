907e782fc0d29f02d976f0f2b15f53b781d76363
Redsparrow77 <jayyzero77@gmail.com>
2022-08-11 08:54:36 +0700

    first commit!

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/*eslint no-undef: "error"*/
+/*eslint-env node*/
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	darkMode: 'class',
+	content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
